586ac0489f9f687efdf160cb197e721612392c62
Cristian Perez <cristian.perez2011@gmail.com>
2019-02-19 15:33:02 +0800

    Complete Exercise_11

--- src/main/java/com/codingnomads/part_02/Exercise_11.java
+++ src/main/java/com/codingnomads/part_02/Exercise_11.java
@@ -1,5 +1,6 @@
 package com.codingnomads.part_02;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
@@ -24,9 +25,29 @@ public class Exercise_11 {
         double invAmt = scanner.nextDouble();
 
         System.out.println("Interest rate as a percentage: ");
-        //TODO: Finish this
-    }
+        try {
 
+            double intRate = scanner.nextDouble();
+            System.out.println("Number of years to invest: ");
+            double years = scanner.nextDouble();
+            double futureAmt = invAmt + (invAmt * (intRate * years));
+            System.out.println("The future value for your initial investment of " + invAmt + " at an interest rate of "
+                    + intRate + " during " + years + " year(s) will be: " + futureAmt);
+        } catch (InputMismatchException exc) {
+
+            String tempIntRate = scanner.nextLine();
+            String newIntRate = tempIntRate.replace("%", "");
+            double intRate = Double.parseDouble(newIntRate) / 100.00;
+
+            System.out.println("Number of years to invest: ");
+            double years = scanner.nextDouble();
+
+            double futureAmt = invAmt + (invAmt * (intRate * years));
+
+            System.out.println("The future value for your initial investment of " + invAmt + " at an interest rate of "
+                    + intRate + " during " + years + " year(s) will be: " + futureAmt);
+        }
+    }
 }
 
 
