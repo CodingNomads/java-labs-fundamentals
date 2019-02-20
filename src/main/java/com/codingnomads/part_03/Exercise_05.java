d71cfeafcc07521648ffa665655075561b5614dd
Cristian Perez <cristian.perez2011@gmail.com>
2019-02-20 21:43:20 +0800

    Complete Exercise_05 and 06 from part_03

--- src/main/java/com/codingnomads/part_03/Exercise_05.java
+++ src/main/java/com/codingnomads/part_03/Exercise_05.java
@@ -1,21 +1,29 @@
 package com.codingnomads.part_03;
 
+import java.util.Scanner;
+
 public class Exercise_05 {
 
     public static void main(String[] strings) throws java.io.IOException {
 
         // read an integer from the user >=1 and <= 999,999,999
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number >=1 and <= 999,999,999: ");
+        int userNum = scanner.nextInt();
 
 
         // use the && operator to see if the user's number is divisible by both 4 and 7
-
-
-        // use the || operator to see if the user's number is divisible by 4 or 7
-
-
-        // use the ^ operator to see if the user's number is divisible for 4 or 7 exclusively
-
-
+        if (userNum % 4 == 0 && userNum % 7 == 0) {
+            System.out.println("it is divisible by 4 and 7");
+        } // use the || operator to see if the user's number is divisible by 4 or 7
+        else if (userNum % 4 == 0 || userNum % 7 == 0) {
+            System.out.println("it is divisible by 4 or 7");
+        } // use the ^ operator to see if the user's number is divisible for 4 or 7 exclusively
+        else if (userNum % 4 == 0 ^ userNum % 7 == 0) {
+            System.out.println("it is divisible for 4 or 7 exclusively");
+        } else {
+            System.out.println("It did not meet any criteria");
+        }
         // print out the results
 
     }
--- src/main/java/com/codingnomads/part_03/Exercise_06.java
+++ src/main/java/com/codingnomads/part_03/Exercise_06.java
@@ -4,30 +4,57 @@ import java.util.Scanner;
 
 class Exercise_06 {
 
+    //Bali Timezone GMT + 8
+
     // the goal of this method is to print out the local time in any given timezone
     public static void main(String[] strings) {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter the time zone offset to GMT (ie, -8 or 8): ");
+        System.out.print("Enter the time zone offset to GMT: ");
+
         long timeZoneChange = input.nextInt();
 
-        // Get total milliseconds since midnight, 1/1/1970
-        long totalMilliseconds = System.currentTimeMillis();
+        long totalMilliseconds = 0;
+
+        if (timeZoneChange < 0 && timeZoneChange > -12) {
+            totalMilliseconds = System.currentTimeMillis() - (timeZoneChange * 3600) * 1000;
+        } else if (timeZoneChange >= 0 && timeZoneChange <= 12) {
+            totalMilliseconds = System.currentTimeMillis() + (timeZoneChange * 3600) * 1000;
+        } else {
+            System.out.println("Invalid Time Zone - Try Again!");
+        }
+
+        printTime(totalMilliseconds);
+
+    }
+
+    public static void printTime(long milli) {
 
         // Get total seconds since midnight, 1/1/1970
+        long totalSeconds = milli / 1000;
 
         // Get the current second within the minute within the hour
+        long secondWithinTheHour = totalSeconds % 60 % 60;
 
         // Get total minutes
+        long totalMinutes = totalSeconds / 60;
 
         // Get the current minute in the hour
+        long minuteWithinTheHour = totalMinutes % 60;
 
         // Get the total hours
+        long totalHours = totalMinutes / 60;
 
         // Get the current hour
+        long currentHour = totalHours % 60;
 
         // Display results using a 12 hour clock, include AM or PM
+
+        String formattedTime = String.format("%02d", currentHour) + ":" + String.format("%02d", minuteWithinTheHour)
+                + ":" + String.format("%02d", secondWithinTheHour);
+
+        System.out.println(formattedTime);
 
     }
 }
