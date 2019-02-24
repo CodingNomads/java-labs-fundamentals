6c7c5a52b195b514b61823bae2517f3eed3022df
Sabine Steiger <sabine.steiger@gmail.com>
2019-02-24 19:42:50 +0800

    ex3 worked on with Caden

--- src/main/java/com/codingnomads/part_04/Exercise_03.java
+++ src/main/java/com/codingnomads/part_04/Exercise_03.java
@@ -1,41 +1,38 @@
 package com.codingnomads.part_04;
 
 /**
-
- Create, populate, and print out a two-dimensional array that will show the following output:
-
- 0 1 2 3 4 5 6 7 8 9
- 10 11 12 13 14 15 16 17 18 19
- 20 21 22 23 24 25 26 27 28 29
- 30 31 32 33 34 35 36 37 38 39
- 40 41 42 43 44 45 46 47 48 49
-
+ * Create, populate, and print out a two-dimensional array that will show the following output:
+ * <p>
+ * 0 1 2 3 4 5 6 7 8 9
+ * 10 11 12 13 14 15 16 17 18 19
+ * 20 21 22 23 24 25 26 27 28 29
+ * 30 31 32 33 34 35 36 37 38 39
+ * 40 41 42 43 44 45 46 47 48 49
  */
 
 public class Exercise_03 {
 
     public static void main(String[] strings) {
 
-        int[][] twoDimensions = new int[9][4];
+
+        int[][] twoDimensions = new int[5][10];
+
+        int counter = 0;
 
         for (int i = 0; i < twoDimensions.length; i++) {
-            for (int t = 0; t < twoDimensions.length; t++) {
-                twoDimensions[i][t] = i + t +i*10;
+            for (int t = 0; t < twoDimensions[i].length; t++) {
 
-              /* if(i == 1) twoDimensions[i][t] = i + t +10; // = +i*10
-                if(i == 2) twoDimensions[i][t] = i + t + 20;
-                if(i == 3)twoDimensions[i][t] = i + t +30;
-                if(i == 4)twoDimensions[i][t] = i + t +40; */
-
+                twoDimensions[i][t] = counter;
+                counter = counter + 1;
             }
         }
         for (int i = 0; i < twoDimensions.length; i++) {
-            for (int t = 0; t < twoDimensions.length; t++) {
+            for (int t = 0; t < twoDimensions[i].length; t++) {
 
-                System.out.println(twoDimensions[i][t]);
-        }
+                System.out.print(twoDimensions[i][t] + " ");
+            }
             System.out.println();
 
         }
-}
+    }
 }
