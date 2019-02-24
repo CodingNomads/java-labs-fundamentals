76d8ee3a1abacf86d2858f909e54e07c209694b5
Sabine Steiger <sabine.steiger@gmail.com>
2019-02-24 22:24:17 +0800

    questions filled up with google n polished #4

--- src/main/java/com/codingnomads/part_04/Exercise_04.java
+++ src/main/java/com/codingnomads/part_04/Exercise_04.java
@@ -13,33 +13,35 @@ class Exercise_04 {
                 {18, -456, 14, 6, 48, -80}
         };
         // uncomment the lines below when your methods are ready
-//        System.out.println(getMaxVal(dataArray));
-//        System.out.println(getMinVal(dataArray));
-   /* }
-
-    // write a method called getMaxVal that takes in a 2D array and iterates through the array to determine the max value in array
-    public void getMaxVal(int[][] dataArray){
-        int max = 0;
-        for(int i = 0; i < dataArray.length; i++){
-            if(dataArray[i] > max)max = dataArray[i];
-            System.out.println(max);
-
-        }
-
+        System.out.println(getMaxVal(dataArray));
+        System.out.println(getMinVal(dataArray));
     }
 
-    // write a method called getMinVal that takes in a 2D array and iterates through the array to determine the min value in array
-    public void getMinVal(int[][] dataArray){
-        int min = 0;
-        for(int i = 0; i < dataArray.length; i++){
-            if(dataArray[i] < min)min = dataArray[i];
-            System.out.println(min);
+    // write a method called getMaxVal that takes in a 2D array and iterates through the array to determine the max value in array
+
+    public static int getMaxVal(int[][] ar){
+        int max = 0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int t = 0; t < ar[i].length; t++) {
+
+                if (ar[i][t] > max)
+                    max = ar[i][t];
+            }
+        }return max;
+    }
+
+// write a method called getMinVal that takes in a 2D array and iterates through the array to determine the min value in array
+
+        public static int getMinVal(int[][] ray){
+            int min = 0;
+            for (int i = 0; i < ray.length; i++) {
+                for (int t = 0; t < ray[i].length; t++) {
+
+                    if (ray[i][t] < min)
+                        min = ray[i][t];
+
+                }
+
+            }return min;
         }
-
-        }
-
-
-          if(nums[i] < min)
-        min = nums[i];
-        if(nums[i] > max)
-        max = nums[i];*/  }}
+    }
